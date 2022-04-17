87040f268438c1bceae86cd5792e0c991a34a02f
uhj1993 <dzzd777@naver.com>
2022-04-17 22:24:26 +0900

    create: followList, NicknameEditForm, Profile

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,7 +1,28 @@
+import React from "react";
+
+import NicknameEditForm from "../components/NicknameEditForm";
 import AppLayout from "../components/AppLayout";
+import FollowList from "../components/FollowList";
 
 const Profile = () => {
-  return <AppLayout>프로필</AppLayout>;
+  const followerList = [
+    { nickname: "지니" },
+    { nickname: "저니" },
+    { nickname: "오피셜" },
+  ];
+  const followingList = [
+    { nickname: "지니" },
+    { nickname: "저니" },
+    { nickname: "오피셜" },
+  ];
+
+  return (
+    <AppLayout>
+      <NicknameEditForm />
+      <FollowList header="팔로잉 목록" data={followingList} />
+      <FollowList header="팔로워 목록" data={followerList} />
+    </AppLayout>
+  );
 };
 
 export default Profile;
