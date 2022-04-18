f7c4bb94bcd0100f7a9e20fca4c4d26d1fc6fe82
uhj1993 <dzzd777@naver.com>
2022-04-18 22:54:24 +0900

    feat: reducer action 설정

---
+++ prepare/front/reducers/index.js
@@ -0,0 +1,67 @@
+import { HYDRATE } from "next-redux-wrapper";
+
+const initiailState = {
+  user: {
+    isLoggedIn: false,
+    user: null,
+    signUpData: {},
+    loginData: {},
+  },
+  post: {
+    mainPosts: [],
+  },
+};
+
+// async action creator
+
+// action creator
+const changeNickname = (data) => {
+  return {
+    type: "CHAMGE_NICKNAME",
+    data,
+  };
+};
+
+export const loginAction = (data) => {
+  return {
+    type: "LOG_IN",
+    data,
+  };
+};
+
+export const logoutAction = (data) => {
+  return {
+    type: "LOG_OUT",
+    data,
+  };
+};
+
+// (이전상태, 액션) => 다음상태
+const rootReducer = (state = initiailState, action) => {
+  switch (action.type) {
+    case HYDRATE:
+      return { ...state, ...action.payload };
+    case "LOG_IN":
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          isLoggedIn: true,
+          user: action.data,
+        },
+      };
+    case "LOG_OUT":
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          isLoggedIn: false,
+          user: null,
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
