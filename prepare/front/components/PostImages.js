4eec7123fe25e696715816c97e9e43c0ce3d3d3a
uhj1993 <dzzd777@naver.com>
2022-04-20 23:17:18 +0900

    feat: PostImage 구현
    - 컴포넌트에 if문에 따라 return문을 다르게 할 수 있다.

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -1,5 +1,81 @@
+import React, { useCallback, useState } from "react";
+import { PlusOutlined } from "@ant-design/icons";
+
 const PostImages = ({ images }) => {
-  return <div>RNGUS</div>;
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+
+  const onClose = useCallback(() => {
+    setShowImagesZoom(false);
+  }, []);
+
+  if (images.length === 1) {
+    return (
+      <>
+        <img
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
+
+  if (images.length === 2) {
+    return (
+      <>
+        <div>
+          <img
+            role="presentation"
+            src={images[0].src}
+            alt={images[0].src}
+            width="50%"
+            onClick={onZoom}
+          />
+          <img
+            role="presentation"
+            src={images[1].src}
+            alt={images[1].src}
+            width="50%"
+            onClick={onZoom}
+          />
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div>
+        <img
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          width="50%"
+          onClick={onZoom}
+        />
+        <div
+          role="presentation"
+          style={{
+            display: "inline-block",
+            width: "50%",
+            textAlign: "center",
+            verticalAlign: "middle",
+          }}
+          onClick={onZoom}
+        >
+          <PlusOutlined />
+          <br />
+          {images.length - 1}
+          개의 사진 더보기
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PostImages;
