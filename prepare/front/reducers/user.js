cdc1779c13f622cf7ac6b2326fb0e70612622db3
uhj1993 <dzzd777@naver.com>
2022-04-20 22:45:22 +0900

    fix: user reducer fix

--- prepare/front/reducers/user.js
+++ prepare/front/reducers/user.js
@@ -1,44 +1,56 @@
 export const initialState = {
   isLoggedIn: false,
-  user: null,
+  me: null,
   signUpData: {},
   loginData: {},
 };
 
+export const SIGN_UP = "SIGN_UP";
+export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
+export const LOG_IN = "LOG_IN"; // 액션의 이름
+export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS"; // 액션의 이름
+export const LOG_IN_FAILURE = "LOG_IN_FAILURE"; // 액션의 이름
+export const LOG_OUT = "LOG_OUT";
+
+export const signUpAction = (data) => {
+  return {
+    type: SIGN_UP,
+    data,
+  };
+};
+
+export const signUpSuccess = {
+  type: SIGN_UP_SUCCESS,
+};
+
 export const loginAction = (data) => {
   return {
-    type: "LOG_IN",
+    type: LOG_IN,
     data,
   };
 };
 
-export const logoutAction = (data) => {
-  return {
-    type: "LOG_OUT",
-    data,
-  };
+export const logoutAction = {
+  type: LOG_OUT,
 };
 
-export default (state = initialState, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "LOG_IN": {
+    case "LOG_IN":
       return {
         ...state,
         isLoggedIn: true,
-        user: action.data,
+        me: action.data,
       };
-    }
-    case "LOG_OUT": {
+    case "LOG_OUT":
       return {
         ...state,
         isLoggedIn: false,
-        user: null,
+        me: null,
       };
-    }
-
     default:
-      return {
-        state,
-      };
+      return state;
   }
 };
+
+export default reducer;
