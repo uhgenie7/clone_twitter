88e63077a10a31337ba66b146bdc053db4dc207b
uhj1993 <dzzd777@naver.com>
2022-04-23 17:39:47 +0900

    feat: redux-saga 설치 및 saga 사용법

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -1,1 +1,76 @@
-export default function* rootSaga() {}
+import { all, fork, call, put } from "redux-saga/effects";
+import axios from "axios";
+
+// 여기서의 매개변수 data는 call의 두 번째 매개변수의 data
+function logInAPI(data) {
+  return axios.post("/api/login", data);
+}
+
+function logOutAPI() {
+  return axios.post("/api/logout");
+}
+
+function addPostAPI(data) {
+  return axios.post("/api/post", data);
+}
+
+function* logIn(action) {
+  try {
+    const result = yield call(logInAPI, action.data);
+    yield put({
+      type: "LOG_IN_SUCCESS",
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: "LOG_IN_FAILURE",
+      data: err.response.data,
+    });
+  }
+}
+
+function* logOut() {
+  try {
+    const result = yield call(logOutAPI);
+    yield put({
+      type: "LOG_OUT_SUCCESS",
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: "LOG_OUT_FAILURE",
+      data: err.response.data,
+    });
+  }
+}
+
+function* addPost(action) {
+  try {
+    const result = yield call(addPostAPI, action.data);
+    yield put({
+      type: "ADD_POST_SUCCESS",
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: "ADD_POST_FAILURE",
+      data: err.response.data,
+    });
+  }
+}
+
+function* watchLogin() {
+  yield take("LOG_IN_REQUEST", logIn);
+}
+
+function* watchLogout() {
+  yield take("LOG_OUT_REQUEST", logOut);
+}
+
+function* watchAddPost() {
+  yield take("ADD_POST_REQUEST", addPost);
+}
+
+export default function* rootSaga() {
+  yield all([fork(watchLogin), fork(watchLogout), fork(watchAddPost)]);
+}
