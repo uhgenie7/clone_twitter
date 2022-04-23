31f3fdb51530ea40cdee03a43a5f26939a895c33
uhj1993 <dzzd777@naver.com>
2022-04-23 23:42:42 +0900

    refactor: saga 리팩토링

--- prepare/front/sagas/post.js
+++ prepare/front/sagas/post.js
@@ -9,6 +9,15 @@ import {
   delay,
 } from "redux-saga/effects";
 import axios from "axios";
+import {
+  ADD_POST_FAILURE,
+  ADD_POST_REQUEST,
+  ADD_POST_SUCCESS,
+  ADD_COMMENT_FAILURE,
+  ADD_COMMENT_REQUEST,
+  ADD_COMMENT_SUCCESS,
+  addComment,
+} from "../reducers/post";
 
 function addPostAPI(data) {
   return axios.post("/api/post", data);
@@ -19,21 +28,45 @@ function* addPost(action) {
     // const result = yield call(addPostAPI, action.data);
     yield delay(1000);
     yield put({
-      type: "ADD_POST_SUCCESS",
-      data: result.data,
+      type: ADD_POST_SUCCESS,
+      // data: result.data,
     });
   } catch (err) {
     yield put({
-      type: "ADD_POST_FAILURE",
-      data: err.response.data,
+      type: ADD_POST_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+function addCommentAPI(data) {
+  return axios.post("/api/post/${data.postId}/comment", data);
+}
+
+function* addComment(action) {
+  try {
+    // const result = yield call(addCommentAPI, action.data);
+    yield delay(1000);
+    yield put({
+      type: ADD_COMMENT_SUCCESS,
+      // data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: ADD_COMMENT_FAILURE,
+      error: err.response.data,
     });
   }
 }
 
 function* watchAddPost() {
-  yield takeLatest("ADD_POST_REQUEST", addPost, 2000);
+  yield takeLatest(ADD_POST_REQUEST, addPost, 2000);
+}
+
+function* watchAddComment() {
+  yield takeLatest(ADD_COMMENT_REQUEST, addComment, 2000);
 }
 
 export default function* postSaga() {
-  yield all([fork(watchAddPost)]);
+  yield all([fork(watchAddPost), fork(watchAddComment)]);
 }
