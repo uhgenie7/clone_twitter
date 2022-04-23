9bbc7db3c088ab3fc116463fd65979829afb96ad
uhj1993 <dzzd777@naver.com>
2022-04-23 16:15:53 +0900

    feat: redux-saga 설치 및 기본세팅
    - rootSaga가 필요하다.
    - 제너레이터의 개념을 알고 있어야 한다.

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -2,7 +2,7 @@ import { createWrapper } from "next-redux-wrapper";
 import { applyMiddleware, compose, createStore } from "redux";
 import reducer from "../reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
-import thunkMiddleware from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 
 const consoleMiddleware =
   ({ dispatch, getState }) =>
@@ -23,12 +23,14 @@ const loggerMiddleware =
   };
 
 const configureStore = () => {
-  const middlewares = [thunkMiddleware, consoleMiddleware];
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware, consoleMiddleware];
   const enhancer =
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
       : composeWithDevTools(applyMiddleware(...middlewares));
   const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 
