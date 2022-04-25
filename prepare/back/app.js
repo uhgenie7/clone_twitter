9d6c51f811b4961fd04f1ebca8b1320ae8bfa549
uhj1993 <dzzd777@naver.com>
2022-04-25 22:46:50 +0900

    invalid: (#6) : 프레임워크 없는 노드
    - 쪼개기가 필요한 코드
    - 서버 쪽 라우터도 쪼개야 한다
    - 기본 노드가 제공하는 http로는 쪼개기가 어렵다.
    - 익스프레스의 필요성!

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,6 +1,21 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   console.log(res.url, req.method);
+
+  if (req.method === "GET") {
+    if (req.url === "/api/posts") {
+      // 뭔가 잘못됐어.
+    }
+  } else if (req.method === "POST") {
+    if (req.url === "/api/posts") {
+      // 뭔가 잘못됐어.
+    }
+  } else if (req.method === "DELETE") {
+    if (req.url === "/api/posts") {
+      // 뭔가 잘못됐어.
+    }
+  }
+
   res.write("<h1>hello node</h1>");
   res.write("hello node");
   res.write("hello node");
