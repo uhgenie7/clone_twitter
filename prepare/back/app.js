24627079dfde8fbfb285e93bd6231a3e2797ee1e
uhj1993 <dzzd777@naver.com>
2022-04-25 22:37:29 +0900

    #8 res, req

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -3,7 +3,6 @@ const server = http.createServer((req, res) => {
   console.log(res.url, req.method);
   res.write("<h1>hello node</h1>");
   res.write("hello node");
-  res.write("hello node");
   res.end("hello node");
 });
 
