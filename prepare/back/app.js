5e9d23cca0aa62c7f0cf516a0f31897a11b55414
uhj1993 <dzzd777@naver.com>
2022-04-29 22:23:04 +0900

    feat: (#13): express에서 시퀄라이즈 등록법

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
-
-const app = express();
 const postRouter = require("./routes/post");
+const db = require("./models");
+const app = express();
+
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("db 연결 성공");
+  })
+  .catch(console.error);
 
 // app.method('url', (req, res)=>{callback})
 app.get("/", (req, res) => {
