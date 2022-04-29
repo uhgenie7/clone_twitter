8de04043764e29ebd71eb5fe9300353f4a8afb02
uhj1993 <dzzd777@naver.com>
2022-04-29 22:07:27 +0900

    feat: #12 - 같은 테이블 내에서의 시퀄라이즈 관계 설정

--- prepare/back/models/user.js
+++ prepare/back/models/user.js
@@ -25,6 +25,16 @@ module.exports = (sequelize, DataTypes) => {
     db.User.hasMany(db.Post);
     db.User.hasMany(db.Comment);
     db.User.belongsToMany(db.Post, { through: "Like", as: "Likers" });
+    db.User.belongsToMany(db.User, {
+      through: "Follow",
+      as: "Followers",
+      foreignKey: "FollowingId",
+    });
+    db.User.belongsToMany(db.User, {
+      through: "Follow",
+      as: "Followings",
+      foreignKey: "FollowerId",
+    });
   };
   return User;
 };
