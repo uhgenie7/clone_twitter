e470e0339b76d60a3a187655b3576da383eae34f
uhj1993 <dzzd777@naver.com>
2022-04-30 01:29:55 +0900

    feat: #14:  암호화 라이브러리 설치

--- prepare/back/routes/user.js
+++ prepare/back/routes/user.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 const { User } = require("../models");
 
-router.post("/", async (req, res) => {
-  // POST /user
-  await User.create({
-    email: req.body.email,
-    nickname: req.body.nickname,
-    password: req.body.password,
-  });
-  res.send("ok");
+router.post("/", async (req, res, next) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 11);
+    // POST /user
+    await User.create({
+      email: req.body.email,
+      nickname: req.body.nickname,
+      password: hashedPassword,
+    });
+    res.send("ok");
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
