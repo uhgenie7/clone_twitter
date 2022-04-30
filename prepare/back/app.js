866b51303e652c9b2930ff3b61ab30814ad0a633
uhj1993 <dzzd777@naver.com>
2022-04-30 21:39:11 +0900

    ✨ #17 dotenv 넣는 방법

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
-
+const passport = require("passport");
+const dotenv = require("dotenv");
 const postRouter = require("./routes/post");
 const userRouter = require("./routes/user");
 const db = require("./models");
 const app = express();
 const passportConfig = require("./passport");
+
+dotenv.config();
 
 db.sequelize
   .sync()
@@ -28,8 +31,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // session, cookie
-app.use(cookieParser());
-app.use(session());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.COOKIE_SECRET,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.seesion());
 
