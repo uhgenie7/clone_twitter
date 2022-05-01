cb10d2c8321aac1c1c06c813158bb850ccc89c09
uhj1993 <dzzd777@naver.com>
2022-05-01 15:23:20 +0900

    ✨ #25 게시물 제거 구현

--- prepare/back/routes/post.js
+++ prepare/back/routes/post.js
@@ -110,9 +110,19 @@ router.delete("/:postId/like", isLoggedIn, async (req, res, next) => {
   }
 });
 
-router.delete("/", (req, res) => {
-  // DELETE /post
-  res.json([{ id: 1 }]);
+router.delete("/:postId", (req, res, next) => {
+  try {
+    await Post.destroy({
+      where: {
+        id: req.params.postId,
+        UserId: req.user.id,
+      },
+    });
+    res.status(200).json({ PostId: parseInt(req.params.postId, 10) });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
