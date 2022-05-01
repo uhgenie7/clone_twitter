8ae178f788d522f9216bc5c8a34e051ddd6a6628
uhj1993 <dzzd777@naver.com>
2022-05-01 21:56:52 +0900

    🐛 #26 팔로우, 언팔로우 bug fixed

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
+import Head from "next/head";
+import { useSelector, useDispatch } from "react-redux";
 import Router from "next/router";
-import NicknameEditForm from "../components/NicknameEditForm";
+
 import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
 import FollowList from "../components/FollowList";
-import { useDispatch, useSelector } from "react-redux";
 import {
   LOAD_FOLLOWERS_REQUEST,
   LOAD_FOLLOWINGS_REQUEST,
@@ -11,17 +13,17 @@ import {
 
 const Profile = () => {
   const dispatch = useDispatch();
+
   const { me } = useSelector((state) => state.user);
 
   useEffect(() => {
     dispatch({
       type: LOAD_FOLLOWERS_REQUEST,
     });
-
     dispatch({
       type: LOAD_FOLLOWINGS_REQUEST,
     });
-  });
+  }, []);
 
   useEffect(() => {
     if (!(me && me.id)) {
@@ -32,23 +34,17 @@ const Profile = () => {
   if (!me) {
     return null;
   }
-  const followerList = [
-    { nickname: "지니" },
-    { nickname: "저니" },
-    { nickname: "오피셜" },
-  ];
-  const followingList = [
-    { nickname: "지니" },
-    { nickname: "저니" },
-    { nickname: "오피셜" },
-  ];
-
   return (
-    <AppLayout>
-      <NicknameEditForm />
-      <FollowList header="팔로잉" data={me.followings} />
-      <FollowList header="팔로워" data={me.followers} />
-    </AppLayout>
+    <>
+      <Head>
+        <title>내 프로필 | NodeBird</title>
+      </Head>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉" data={me.Followings} />
+        <FollowList header="팔로워" data={me.Followers} />
+      </AppLayout>
+    </>
   );
 };
 
