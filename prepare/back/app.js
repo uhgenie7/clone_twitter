b02cc3fcfa248391115f9d6e344b5af3478242d6
uhj1993 <dzzd777@naver.com>
2022-05-01 14:03:22 +0900

    ♻️ #23 주석 정리

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -46,25 +46,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.method('url', (req, res)=>{callback})
-app.get("/", (req, res) => {
-  // end (x) send (o)
-  res.send("hello express");
-});
-
-app.get("/api", (req, res) => {
-  // 문자열 응답
-  res.send("hello api");
-});
-
-app.get("/api/post", (req, res) => {
-  res.json([
-    { id: 1, content: "hello" },
-    { id: 2, content: "hello2" },
-    { id: 3, content: "hello3" },
-  ]);
-});
-
 // pre fix 됨
 app.use("/post", postRouter);
 app.use("/posts", postsRouter);
