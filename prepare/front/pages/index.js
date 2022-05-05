d032564a351c5421ce4337322983a9ce8b26a2da
uhj1993 <dzzd777@naver.com>
2022-05-05 15:40:09 +0900

    🚑 #33 사용자간 쿠키가 공유되지 않게 FIX

--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -64,7 +64,12 @@ const Home = () => {
 export const getServerSideProps = wrapper.getServerSideProps(
   async (context) => {
     const cookie = context.req ? context.req.headers.cookie : "";
-    axios.defaults.headers.Cookie = cookie;
+
+    axios.defaults.headers.Cookie = "";
+
+    if (context.req && cookie) {
+      axios.defaults.headers.Cookie = cookie;
+    }
 
     context.store.dispatch({
       type: LOAD_MY_INFO_REQUEST,
